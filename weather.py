62f80de12ef1e059326ee4f72fffc669521c32a2
Marek Nagy <62247071@cuni.cz>
2020-01-10 09:57:21 +0100

    Added argument options

--- weather.py
+++ weather.py
@@ -9,115 +9,145 @@ from requests import request
 from datetime import datetime
 from time import sleep
 from os import sep
-from sys import platform
+from sys import platform, argv
 
-headers = {
-        'accept': "application/json",
-        'content-type': "application/json"
-        }
+def Main():
 
-response = request("GET", "https://freegeoip.app/json/", headers=headers)
+	args = argv
+	args.remove(args[0])
 
-respDict = response.json()
+	if ArgsCheck(args):
+		headers = {
+				'accept': "application/json",
+				'content-type': "application/json"
+				}
 
-latitude = respDict['latitude']
-longitude = respDict['longitude']
+		response = request("GET", "https://freegeoip.app/json/", headers=headers)
 
-del headers, response, respDict
+		respDict = response.json()
 
-DarkSkySecKey = 0
-SecKeyFilePath = ''
+		latitude = respDict['latitude']
+		longitude = respDict['longitude']
 
-if platform == 'win32':
-    SecKeyFilePath = "SecKey.txt"
-elif platform == 'linux':
-    SecKeyFilePath = "/home/marek/python/WeatherApp/SecKey.txt"
+		del headers, response, respDict
 
-with open(SecKeyFilePath,'r') as file:
-    DarkSkySecKey = file.readline()
+		DarkSkySecKey = 0
+		SecKeyFilePath = ''
 
-response = request("GET", "https://api.darksky.net/forecast/{}/{},{}?units=si".format(DarkSkySecKey, latitude, longitude))
-d = response.json()
-del response
-try:
-    print("Location: {}".format(d['timezone']))
-    cur = d['currently']
+		if platform == 'win32':
+			SecKeyFilePath = "SecKey.txt"
+		elif platform == 'linux':
+			SecKeyFilePath = "/home/marek/python/WeatherApp/SecKey.txt"
 
-    print()
-    print("Currently:")
-    print("Time: {}".format(datetime.fromtimestamp(cur['time'])))
-    print("Apparent temperature: {}°C".format(cur['apparentTemperature']))
-    print("Temperature: {}°C".format(cur['temperature']))
-    print("Wind speed: {} m/s".format(cur['windSpeed']))
-    if cur['windSpeed'] > 0:
-        wd = cur['windBearing']
-        if wd < 45/2 or wd > 360-45/2:
-            wd = "N"
-        elif wd > 45/2 and wd < 3*45/2:
-            wd = "NW"
-        elif wd > 3*45/2 and wd < 5*45/2:
-            wd = "W"
-        elif wd > 5*45/2 and wd < 7*45/2:
-            wd = "SW"
-        elif wd > 7*45/2 and wd < 9*45/2:
-            wd = "S"
-        elif wd > 9*45/2 and wd < 11*45/2:
-            wd = "SE"
-        elif wd > 11*45/2 and wd < 13*45/2:
-            wd = "E"
-        elif wd > 13*45/2 and wd < 15*45/2:
-            wd = "NE"
-        print("Wind direction: {}".format(wd))
-    print("Humidity: {} %".format(cur['humidity']*100))
-    print("Visibility: {} km".format(cur['visibility']))
-    print("Summary: {}".format(cur['summary']))
+		with open(SecKeyFilePath,'r') as file:
+			DarkSkySecKey = file.readline()
 
-    print()
-    hours = 0
-    hours = int(input("Set amount of hours: "))
-    if hours <= 0:
-        raise Exception("Invalid number of hours")
-    hours = min(hours, 47)
-    print("Hourly:")
-    h = d['hourly']
-    dataArr = h['data']
+		response = request("GET", "https://api.darksky.net/forecast/{}/{},{}?units=si".format(DarkSkySecKey, latitude, longitude))
+		d = response.json()
+		del response
+		try:
 
-    for i in range(1, hours + 1):
-        dat = dataArr[i]
-        t = datetime.fromtimestamp(dat['time'])
-        print("Time: {}.{}.{} {}".format( t.day, t.month, t.year, t.time()))
-        print("Apparent temperature: {}°C".format(dat['apparentTemperature']))
-        print("Temperature: {}°C".format(dat['temperature']))
-        print("Wind speed: {} m/s".format(dat['windSpeed']))
-        if dat['windSpeed'] > 0:
-            wd = cur['windBearing']
-            if wd < 45/2 or wd > 360-45/2:
-                wd = "N"
-            elif wd > 45/2 and wd < 3*45/2:
-                wd = "NW"
-            elif wd > 3*45/2 and wd < 5*45/2:
-                wd = "W"
-            elif wd > 5*45/2 and wd < 7*45/2:
-                wd = "SW"
-            elif wd > 7*45/2 and wd < 9*45/2:
-                wd = "S"
-            elif wd > 9*45/2 and wd < 11*45/2:
-                wd = "SE"
-            elif wd > 11*45/2 and wd < 13*45/2:
-                wd = "E"
-            elif wd > 13*45/2 and wd < 15*45/2:
-                wd = "NE"
-            print("Wind direction: {}".format(wd))
-        print("Humidity: {} %".format(dat['humidity']*100))
-        print("Visibility: {} km".format(dat['visibility']))
-        if i < hours:
-            sleep(2)
-        print()
 
-    print("Powered by Dark Sky. ( https://darksky.net/poweredby )")
-except KeyError:
-    print("Error type {}: {}".format(d['code'], d['error']))
-except:
-    print("You're kidding me, right?")
-    print()
-    print("Powered by Dark Sky. ( https://darksky.net/poweredby )")
+			printedInfoCount = 0
+
+			# Used for debugging in terminal
+			#print("args: {}".format(str(args)))
+
+
+			print("Location: {}".format(d['timezone']))
+			if "-c" in args or len(args) == 0:
+				cur = d['currently']
+
+				print()
+				print("Currently:")
+				print("Time: {}".format(datetime.fromtimestamp(cur['time'])))
+				print("Apparent temperature: {}°C".format(cur['apparentTemperature']))
+				print("Temperature: {}°C".format(cur['temperature']))
+				print("Wind speed: {} m/s".format(cur['windSpeed']))
+				if cur['windSpeed'] > 0:
+					wd = cur['windBearing']
+					if wd < 45/2 or wd > 360-45/2:
+						wd = "N"
+					elif wd > 45/2 and wd < 3*45/2:
+						wd = "NW"
+					elif wd > 3*45/2 and wd < 5*45/2:
+						wd = "W"
+					elif wd > 5*45/2 and wd < 7*45/2:
+						wd = "SW"
+					elif wd > 7*45/2 and wd < 9*45/2:
+						wd = "S"
+					elif wd > 9*45/2 and wd < 11*45/2:
+						wd = "SE"
+					elif wd > 11*45/2 and wd < 13*45/2:
+						wd = "E"
+					elif wd > 13*45/2 and wd < 15*45/2:
+						wd = "NE"
+					print("Wind direction: {}".format(wd))
+				print("Humidity: {} %".format(cur['humidity']*100))
+				print("Visibility: {} km".format(cur['visibility']))
+				print("Summary: {}".format(cur['summary']))
+				printedInfoCount += 1
+
+
+			if "-h" in args and args.index("-h") < len(args) - 1:
+				print()
+				hours = int(args[args.index("-h") + 1])
+				if hours <= 0:
+					raise Exception("Invalid number of hours")
+				hours = min(hours, 47)
+				print("Hourly:")
+				h = d['hourly']
+				dataArr = h['data']
+
+				for i in range(1, hours + 1):
+					dat = dataArr[i]
+					t = datetime.fromtimestamp(dat['time'])
+					print("Time: {}.{}.{} {}".format( t.day, t.month, t.year, t.time()))
+					print("Apparent temperature: {}°C".format(dat['apparentTemperature']))
+					print("Temperature: {}°C".format(dat['temperature']))
+					print("Wind speed: {} m/s".format(dat['windSpeed']))
+					if dat['windSpeed'] > 0:
+						wd = dat['windBearing']
+						if wd < 45/2 or wd > 360-45/2:
+							wd = "N"
+						elif wd > 45/2 and wd < 3*45/2:
+							wd = "NW"
+						elif wd > 3*45/2 and wd < 5*45/2:
+							wd = "W"
+						elif wd > 5*45/2 and wd < 7*45/2:
+							wd = "SW"
+						elif wd > 7*45/2 and wd < 9*45/2:
+							wd = "S"
+						elif wd > 9*45/2 and wd < 11*45/2:
+							wd = "SE"
+						elif wd > 11*45/2 and wd < 13*45/2:
+							wd = "E"
+						elif wd > 13*45/2 and wd < 15*45/2:
+							wd = "NE"
+						print("Wind direction: {}".format(wd))
+					print("Humidity: {} %".format(dat['humidity']*100))
+					print("Visibility: {} km".format(dat['visibility']))
+					if i < hours:
+						sleep(2)
+						print()
+				printedInfoCount += 1
+
+			if printedInfoCount > 0:
+				print()
+				print("Powered by Dark Sky. ( https://darksky.net/poweredby )")
+		except KeyError:
+			print("Error type {}: {}".format(d['code'], d['error']))
+		except:
+			print("You're kidding me, right?")
+			print()
+			print("Powered by Dark Sky. ( https://darksky.net/poweredby )")
+	else:
+		print("Invalid arguments Error")
+
+
+def ArgsCheck(args: list):
+	b = True
+	return b
+
+if __name__ == "__main__":
+	Main()
